ad4484782e1b52380e78f63814e0c8ca61850db1
Anel <44476653+anme98@users.noreply.github.com>
2024-12-19 07:16:39 +0100

    project init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ag-grid' title`, () => {
+  it(`should have the 'data-grid' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ag-grid');
+    expect(app.title).toEqual('data-grid');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ag-grid');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, data-grid');
   });
 });
